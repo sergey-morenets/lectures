8c0f243650c22cf6ecc2983adf5fc9830401a607
Sergiy <sergey.morenets@gmail.com>
2022-06-06 21:07:38 +0300

    Lecture 2. Add sample domain entities

---
+++ lecture2/src/main/java/capgemini/spring/Message.java
@@ -0,0 +1,12 @@
+package capgemini.spring;
+
+public class Message {
+
+    private String text;
+
+    private String subject;
+
+    private User sender;
+
+    private User recipient;
+}
---
+++ lecture2/src/main/java/capgemini/spring/User.java
@@ -0,0 +1,22 @@
+package capgemini.spring;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String login;
+
+    private List<Message> messages;
+
+    public void activate() {
+
+    }
+
+    public void block() {
+
+    }
+}
