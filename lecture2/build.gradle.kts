fca3c403e0715e9f6d51b8ceb0f29cee3b5966dd
Sergiy <sergey.morenets@gmail.com>
2022-06-06 20:50:39 +0300

    Lecture 2. Add Spring Test dependency

--- lecture2/build.gradle.kts
+++ lecture2/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework:spring-context:5.3.20")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
+    testImplementation("org.springframework:spring-test:5.3.20")
 
 
 }
---
+++ lecture2/src/test/java/capgemini/spring/ProductAPITest.java
@@ -0,0 +1,20 @@
+package capgemini.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringJUnitConfig
+class ProductAPITest {
+
+    @Autowired
+    ProductAPI productAPI;
+
+    @Test
+    void findProduct_validIdentifier_productReturned() {
+        Product product = productAPI.findProduct(1);
+        assertNotNull(product);
+    }
+}
