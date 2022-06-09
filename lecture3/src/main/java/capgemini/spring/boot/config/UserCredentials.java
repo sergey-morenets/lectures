7126d05344aebdc10eda4824de49a458ccdb0e0c
Sergiy <sergey.morenets@gmail.com>
2022-06-09 20:06:34 +0300

    Lecture 3. Add user credentials

---
+++ lecture3/src/main/java/capgemini/spring/boot/config/UserCredentials.java
@@ -0,0 +1,13 @@
+package capgemini.spring.boot.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCredentials {
+
+    private String login;
+
+    private String password;
+}
