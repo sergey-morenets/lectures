c4ea97585be649259d11802aa415a1b3a6df0f08
Sergiy <sergey.morenets@gmail.com>
2022-06-09 20:11:16 +0300

    Lecture 3. User @Value to inject properties

--- lecture3/src/main/java/capgemini/spring/boot/MainApplication.java
+++ lecture3/src/main/java/capgemini/spring/boot/MainApplication.java
@@ -1,12 +1,19 @@
 package capgemini.spring.boot;
 
+import capgemini.spring.boot.config.UserCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public UserCredentials userCredentials() {
+        return new UserCredentials();
     }
 }
--- lecture3/src/main/java/capgemini/spring/boot/config/UserCredentials.java
+++ lecture3/src/main/java/capgemini/spring/boot/config/UserCredentials.java
@@ -2,12 +2,24 @@ package capgemini.spring.boot.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
 
 @Getter
 @Setter
+@Slf4j
 public class UserCredentials {
-
+    @Value("${app.login}")
     private String login;
 
+    @Value("${app.password}")
     private String password;
+
+    @PostConstruct
+    public void init() {
+        //System.out.println("Our credentials are: login - " + );
+        log.info("User login: {}, password: {}", login, password);
+    }
 }
