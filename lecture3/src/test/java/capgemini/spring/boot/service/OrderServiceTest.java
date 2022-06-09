486bdc1d93a747fe6eedb65f32d7e659c4a3d47d
Sergiy <sergey.morenets@gmail.com>
2022-06-09 20:35:07 +0300

    Lecture 3. Add mocking to write pure unit-tests

--- lecture3/src/test/java/capgemini/spring/boot/service/OrderServiceTest.java
+++ lecture3/src/test/java/capgemini/spring/boot/service/OrderServiceTest.java
@@ -1,22 +1,34 @@
 package capgemini.spring.boot.service;
 
-import capgemini.spring.boot.MainApplication;
 import capgemini.spring.boot.model.Order;
+import capgemini.spring.boot.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-@SpringJUnitConfig(classes = MainApplication.class)
+//@SpringJUnitConfig(classes = MainApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class OrderServiceTest {
+
+    @MockBean
+    OrderRepository orderRepository;
 
     @Autowired
     OrderService orderService;
 
     @Test
     void findById_validId_success() {
+        Order ourOrder = new Order();
+        BDDMockito.given(orderRepository.findById(1)).willReturn(ourOrder);
+
         Order order = orderService.findById(1);
-        assertNotNull(order);
+        // assertNotNull(order);
+        assertSame(ourOrder, order);
+
+        BDDMockito.verify(orderRepository).findById(1);
     }
 }
