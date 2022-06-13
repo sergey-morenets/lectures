595e4de86e898eefe672b5b109400fb7c40027b7
Sergiy <sergey.morenets@gmail.com>
2022-06-13 20:29:05 +0300

    Lecture 3. Add skeleton Docker image

--- lecture1/src/main/java/java8/Calculator.java
+++ lecture1/src/main/java/java8/Calculator.java
@@ -2,6 +2,19 @@ package java8;
 
 public class Calculator {
 
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.divide(3, 2);
+        System.out.println(result);
+    }
+
+    public int divide(int firstOp, int secondOp) {
+        if (secondOp == 0) {
+            throw new IllegalArgumentException("Zero is not allowed as second argument");
+        }
+        return firstOp / secondOp;
+    }
+
     public long plus(int x, int y) {
         return (long) x + y;
     }
