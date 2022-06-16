e25e7a90135583305c2121a8d55e6d4bdc0c61ba
Sergiy <sergey.morenets@gmail.com>
2022-06-16 20:42:32 +0300

    Lecture 5. Add ProductService test

--- lecture5/src/main/java/capgemini/service/ProductService.java
+++ lecture5/src/main/java/capgemini/service/ProductService.java
@@ -3,10 +3,12 @@ package capgemini.service;
 import capgemini.data.model.Product;
 import capgemini.persistence.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
--- lecture5/src/test/java/capgemini/service/ProductServiceTest.java
+++ lecture5/src/test/java/capgemini/service/ProductServiceTest.java
@@ -1,13 +1,36 @@
 package capgemini.service;
 
+import capgemini.SpringDataApplication;
+import capgemini.data.model.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@DataJpaTest
+@SpringBootTest(classes = SpringDataApplication.class)
 class ProductServiceTest {
 
+    @Autowired
+    ProductService productService;
+
     @Test
-    void save() {
+    void save_productWithoutOrders_success() {
+        Product product = new Product();
+        product.setName("Training course");
+        product.setPrice(100);
+
+        productService.save(product);
+        assertTrue(product.getId() > 0);
+
+        List<Product> products = productService.findAll();
+        assertEquals(1, products.size());
+        assertEquals(product.getId(), products.get(0).getId());
+
     }
 
     @Test
