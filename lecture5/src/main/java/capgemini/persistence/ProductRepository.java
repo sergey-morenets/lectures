b9cd8efeea77eaa1f809a5e84b56363083f51650
Sergiy <sergey.morenets@gmail.com>
2022-06-20 19:21:00 +0300

    Lecture 6. Spring Data query methods and JPQL

--- lecture5/src/main/java/capgemini/persistence/ProductRepository.java
+++ lecture5/src/main/java/capgemini/persistence/ProductRepository.java
@@ -2,6 +2,18 @@ package capgemini.persistence;
 
 import capgemini.data.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+
+    List<Product> findByNameAndPrice(String name, Double price);
+
+    //@Query("FROM Product p WHERE p.name = :name and price > :price")
+    @Query(value = "SELECT * FROM PRODUCTS WHERE name=:name and price > :price", nativeQuery = true)
+    List<Product> findSpecific(@Param("name") String name, @Param("price") Double price);
 }
