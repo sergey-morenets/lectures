7499d65c461008584ea961d376a058ca3ed01490
Sergiy <sergey.morenets@gmail.com>
2022-06-23 20:45:42 +0300

    Lecture 7. Add integration-tests

--- lecture7/src/main/java/capgemini/nosql/redis/model/User.java
+++ lecture7/src/main/java/capgemini/nosql/redis/model/User.java
@@ -1,9 +1,13 @@
 package capgemini.nosql.redis.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("users")
+@Setter
+@Getter
 public class User {
 
     @Id
--- lecture7/src/test/java/capgemini/nosql/redis/persistence/UserRepositoryTest.java
+++ lecture7/src/test/java/capgemini/nosql/redis/persistence/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package capgemini.nosql.redis.persistence;
 
 import capgemini.nosql.redis.RedisApplication;
+import capgemini.nosql.redis.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = RedisApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserRepositoryTest {
@@ -12,7 +16,16 @@ class UserRepositoryTest {
     UserRepository userRepository;
 
     @Test
-    public void contextLoads() {
+    public void findAll_keysExist_success() {
+        User user = new User();
+        user.setId(1);
+        user.setAge(30);
+        user.setName("Jack");
+        userRepository.save(user);
 
+        Iterable<User> users = userRepository.findAll();
+        assertTrue(users.iterator().hasNext());
+        User user1 = users.iterator().next();
+        assertEquals(user.getId(), user1.getId());
     }
 }
