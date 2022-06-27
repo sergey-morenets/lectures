4c2c1972f23a0998f48449e8664471eb8a3fdfb9
Sergiy <sergey.morenets@gmail.com>
2022-06-27 22:13:53 +0300

    Lecture 8. Fix Docker Compose Kafka configuration and added default constructor for event

--- lecture8/src/main/java/capgemini/kafka/event/UserRegisteredEvent.java
+++ lecture8/src/main/java/capgemini/kafka/event/UserRegisteredEvent.java
@@ -2,25 +2,22 @@ package capgemini.kafka.event;
 
 import capgemini.kafka.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class UserRegisteredEvent {
 
     private int userId;
-
-    private LocalDateTime registeredAt;
 
     private String email;
 
     public UserRegisteredEvent(User user) {
         userId = user.getId();
         email = user.getEmail();
-        registeredAt = LocalDateTime.now();
     }
 }
