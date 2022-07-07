fb8d064ff84364c06af1495d3b5db319effa91ba
Sergiy <sergey.morenets@gmail.com>
2022-07-07 19:17:44 +0300

    Lecture 11. Add @EnableCaching

--- lecture11/src/main/java/capgemini/cloud/CloudApplication.java
+++ lecture11/src/main/java/capgemini/cloud/CloudApplication.java
@@ -2,8 +2,10 @@ package capgemini.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CloudApplication {
 
     public static void main(String[] args) {
