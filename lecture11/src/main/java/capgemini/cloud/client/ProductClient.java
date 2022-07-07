b34b7807090da55f86d9ec396f94a97d4410970d
Sergiy <sergey.morenets@gmail.com>
2022-07-07 20:58:04 +0300

    Lecture 11. Add Spring Cloud OpenFeign configuration

--- lecture11/src/main/java/capgemini/cloud/client/ProductClient.java
+++ lecture11/src/main/java/capgemini/cloud/client/ProductClient.java
@@ -1,5 +1,6 @@
 package capgemini.cloud.client;
 
+import capgemini.cloud.config.FeignConfiguration;
 import capgemini.cloud.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 //@FeignClient(url = "http://product:8090/products")
-@FeignClient("product")
+@FeignClient(name = "product", configuration = FeignConfiguration.class)
 public interface ProductClient {
 
     @GetMapping
---
+++ lecture11/src/main/java/capgemini/cloud/config/FeignConfiguration.java
@@ -0,0 +1,19 @@
+package capgemini.cloud.config;
+
+import feign.Contract;
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+    @Bean
+    public Contract feignContract() {
+        return new feign.Contract.Default();
+    }
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("user", "password");
+    }
+}
