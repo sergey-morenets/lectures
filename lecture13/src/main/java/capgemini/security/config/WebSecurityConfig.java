ed875faa7838f53e59f6e31277108da81138c819
Sergiy <sergey.morenets@gmail.com>
2022-07-14 21:08:35 +0300

    Lecture 13. Add user details service and password encoder

--- lecture13/src/main/java/capgemini/security/config/WebSecurityConfig.java
+++ lecture13/src/main/java/capgemini/security/config/WebSecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +21,34 @@ public class WebSecurityConfig {
         http.httpBasic();
         http.formLogin();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        var userDetailsService =
+                new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("user")
+                .password("user")
+                .passwordEncoder(passwordEncoder::encode)
+                .authorities("USER", "ADMIN")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        var admin = User.withUsername("admin")
+                .password("admin")
+                .passwordEncoder(passwordEncoder::encode)
+                .authorities("ADMIN")
+                .build();
+
+        userDetailsService.createUser(admin);
+
+        return userDetailsService;
     }
 }
