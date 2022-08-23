438dc9ad7814561fa83434974c3d1704863fc3f8
Sergiy <sergey.morenets@gmail.com>
2022-08-23 11:47:06 +0300

    JUnit 5 usage

---
+++ src/main/java/capgemini/lecture1/Calculator.java
@@ -0,0 +1,11 @@
+package capgemini.lecture1;
+
+//Thread-safe
+public class Calculator {
+
+    public int sum(int firstOp, int secondOp) {
+        return firstOp + secondOp;
+    }
+
+
+}
---
+++ src/test/java/capgemini/lecture1/CalculatorTest.java
@@ -0,0 +1,31 @@
+package capgemini.lecture1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("When we add two positive numbers we receive positive number")
+        //Given-When-Then
+    void sum_twoPositiveNumbers_positiveNumber() {
+        assertTrue(calculator.sum(1, 2) > 0);
+    }
+
+    @Test
+    @DisplayName("When we add two big positive numbers we receive positive number")
+        //Given-When-Then
+    void sum_twoBigPositiveNumbers_positiveNumber() {
+        assertTrue(calculator.sum(Integer.MAX_VALUE, Integer.MAX_VALUE) > 0);
+    }
+}
