0f8e58dd9884f69ce7cfe5f1a2bdafe808e07d9c
Sergiy <sergey.morenets@gmail.com>
2022-08-30 10:52:00 +0300

    Lecture 3. LISP principle

--- lecture2/src/main/java/capgemini/spring/model/Order.java
+++ lecture2/src/main/java/capgemini/spring/model/Order.java
@@ -1,4 +1,11 @@
 package capgemini.spring.model;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(of = "id")
 public class Order {
+
+    private int id;
+
+
 }
--- lecture2/src/main/java/capgemini/spring/model/Product.java
+++ lecture2/src/main/java/capgemini/spring/model/Product.java
@@ -1,5 +1,8 @@
 package capgemini.spring.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Product {
 
     private int id;
@@ -8,7 +11,24 @@ public class Product {
 
     private double price;
 
+    //private ArrayList<Order> orders;
+
+    private Set<Order> orders;
+
+    //SOLID principles
+
+    //1.Set<Order> orders
+    //2.HashSet<Order> orders
+    //3.List<Order> orders
+    //4.ArrayList<Order> orders
+    //5.Collection<Order> orders
+    //6.List<Integer> orders
+
     public void addOrder(Order order) {
+
+        orders = new HashSet<>();
+        orders.add(order);
+        orders.add(order);
 
     }
 }
