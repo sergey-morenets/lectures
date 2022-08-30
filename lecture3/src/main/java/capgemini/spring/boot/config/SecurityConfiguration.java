b1254a5719b9fd589e44a9566bafa8777119cc8b
Sergiy <sergey.morenets@gmail.com>
2022-08-30 11:43:43 +0300

    Lecture 3. Configuration mapping. Step #4 with Java records

--- lecture3/src/main/java/capgemini/spring/boot/config/SecurityConfiguration.java
+++ lecture3/src/main/java/capgemini/spring/boot/config/SecurityConfiguration.java
@@ -1,23 +1,11 @@
 package capgemini.spring.boot.config;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 import javax.annotation.PostConstruct;
 
-@Getter
-@RequiredArgsConstructor
-@ToString
-@ConstructorBinding
 @ConfigurationProperties("security")
-public class SecurityConfiguration {
-
-    private final String secretKey;
-
-    private final String secretToken;
+public record SecurityConfiguration(String secretKey, String secretToken) {
 
     @PostConstruct
     void init() {
