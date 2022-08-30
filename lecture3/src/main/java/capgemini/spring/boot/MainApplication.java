f1ef2a900b8d9d0cbfbac19d79cc37fb4b96b271
Sergiy <sergey.morenets@gmail.com>
2022-08-30 11:41:19 +0300

    Lecture 3. Configuration mapping. Step #3

--- lecture3/src/main/java/capgemini/spring/boot/MainApplication.java
+++ lecture3/src/main/java/capgemini/spring/boot/MainApplication.java
@@ -3,19 +3,19 @@ package capgemini.spring.boot;
 import capgemini.spring.boot.config.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SecurityConfiguration.class)
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
-    @Bean
-    SecurityConfiguration securityConfiguration(Environment env) {
-        return new SecurityConfiguration(env.getRequiredProperty("secretKey"),
-                env.getProperty("secretToken"));
-    }
+//    @Bean
+//    SecurityConfiguration securityConfiguration(Environment env) {
+//        return new SecurityConfiguration(env.getRequiredProperty("secretKey"),
+//                env.getProperty("secretToken"));
+//    }
 }
