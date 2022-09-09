701d5ec99ff4130aa0942a861d032324807569d0
Sergiy <sergey.morenets@gmail.com>
2022-09-09 21:05:03 +0300

    Lecture 6. Added integration tests for REST controllers

---
+++ lecture6/src/test/java/capgemini/web/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package capgemini.web.controller;
+
+import capgemini.web.dto.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    JacksonTester<UserDTO> jacksonTester;
+
+    @Test
+    void findAll_dataStoreNotEmpty_success() throws Exception {
+        ResultActions actions = mockMvc.perform(get("/users"));
+        actions.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+//        MvcResult mvcResult = actions.andReturn();
+//        mvcResult.getResponse().getContentAsString();
+//        mvcResult.getResponse().getStatus();
+    }
+
+    @Test
+    void register_validUser_success() throws Exception {
+        UserDTO userDTO = new UserDTO(1, "jack", 50);
+
+        ResultActions actions = mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON).content(jacksonTester.write(userDTO).getJson()));
+        actions.andExpect(status().isCreated());
+    }
+}
