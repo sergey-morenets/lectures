c3b319cbe63949118e0fc4832583757db9f12295
Sergiy <sergey.morenets@gmail.com>
2022-09-09 20:25:46 +0300

    Lecture 6. Added more REST-services

--- lecture6/src/main/java/capgemini/web/controller/UserController.java
+++ lecture6/src/main/java/capgemini/web/controller/UserController.java
@@ -1,16 +1,49 @@
 package capgemini.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import capgemini.web.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("users")
 // GET /
 public class UserController {
 
-    @GetMapping("hello")
-    public String hello() {
-        return "Hello, Capgemini!";
+    private final ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+
+    @GetMapping
+    public List<User> findAll() {
+        return List.of(new User(1, "john", "john", 30),
+                new User(2, "peter", "peter", 40));
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody User user) {
+        System.out.println("Success! " + user.getUsername());
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable int id, @RequestBody User user) {
+        System.out.println("Updated user with id " + id);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        System.out.println("Deleted user with id " + id);
+    }
+
+//    @GetMapping
+//    public String findAll() throws JsonProcessingException {
+//        return objectMapper.writeValueAsString(List.of(new User(1, "john", "john", 30),
+//                new User(2, "peter", "peter", 40)));
+//    }
+
+//    @GetMapping("hello")
+//    public String hello() {
+//        return "Hello, Capgemini!";
+//    }
 }
--- lecture6/src/main/java/capgemini/web/model/User.java
+++ lecture6/src/main/java/capgemini/web/model/User.java
@@ -1,10 +1,14 @@
 package capgemini.web.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private int id;
