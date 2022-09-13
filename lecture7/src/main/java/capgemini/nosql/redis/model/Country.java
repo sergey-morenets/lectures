495719c990a39ebd902740e0aa89f886f30591ba
Sergiy <sergey.morenets@gmail.com>
2022-09-13 11:36:14 +0300

    Lecture 7. Fix serialization issue

--- lecture7/src/main/java/capgemini/nosql/redis/model/Country.java
+++ lecture7/src/main/java/capgemini/nosql/redis/model/Country.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Country {
+//TODO investigate how to avoid Serializable interface
+public class Country implements Serializable {
 
     private int id;
 
