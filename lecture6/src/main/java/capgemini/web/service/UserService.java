30a502a261f23658eca30bd3a6c076396ac7f30c
Sergiy <sergey.morenets@gmail.com>
2022-09-23 19:18:06 +0300

    Lecture 10. ResponseStatusException

--- lecture6/src/main/java/capgemini/web/service/UserService.java
+++ lecture6/src/main/java/capgemini/web/service/UserService.java
@@ -1,10 +1,11 @@
 package capgemini.web.service;
 
-import capgemini.web.error.handling.NotFoundException;
 import capgemini.web.model.User;
 import capgemini.web.persistence.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +14,9 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User findById(int id) {
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User with id " + id + " not found"));
+        //return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User with id " + id + " not found"));
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "User with id " + id + " not found"));
     }
 
 
