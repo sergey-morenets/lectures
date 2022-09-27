72e3ec8dec76f214613a623d55b4c60c01176f29
Sergiy <sergey.morenets@gmail.com>
2022-09-27 10:36:58 +0300

    Lecture 11. Integration tests for creating countries

--- lecture7/src/test/java/capgemini/nosql/redis/web/CountryControllerTest.java
+++ lecture7/src/test/java/capgemini/nosql/redis/web/CountryControllerTest.java
@@ -4,27 +4,35 @@ import capgemini.nosql.redis.model.Country;
 import capgemini.nosql.redis.service.CountryService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class CountryControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
+    JacksonTester<Country> jacksonTester;
+
+    //    @MockBean
     CountryService countryService;
 
     @Test
@@ -37,5 +45,18 @@ class CountryControllerTest {
         actions.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("Ukraine")));
         BDDMockito.verify(countryService).findById(countryId);
+    }
+
+    @Test
+    @DisplayName("POST /countries will create new country")
+    void create_validCountry_success() throws Exception {
+        Country country = new Country();
+        country.setName("Ukraine");
+        mockMvc.perform(post("/countries").contentType(MediaType.APPLICATION_JSON)
+                        .content(jacksonTester.write(country).getJson())).andExpect(status().isCreated())
+                .andExpect((header().exists(HttpHeaders.LOCATION)));
+        //TODO check that country was created
+        int countryId = 0; //take from Location header
+        mockMvc.perform(get("/countries/" + countryId));
     }
 }
