3476ad1cd0178f6d791596ba39fb7862474cd2dc
Sergiy <sergey.morenets@gmail.com>
2022-09-27 11:38:04 +0300

    Lecture 11. Spring Cloud Load Balancer

--- lecture8/lecture8-test/src/main/java/capgemini/test/TestApplication.java
+++ lecture8/lecture8-test/src/main/java/capgemini/test/TestApplication.java
@@ -2,7 +2,10 @@ package capgemini.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +13,11 @@ public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
+++ lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
@@ -1,17 +1,19 @@
 package capgemini.test.service;
 
 import capgemini.user.dto.StudentDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class ExamService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     private String getStudentName(int studentId) {
         //TODO how to use response class from other microservices ?
-        StudentDTO student = restTemplate.getForObject("http://localhost:8080/students/" + studentId,
+        StudentDTO student = restTemplate.getForObject("http://user/students/" + studentId,
                 StudentDTO.class);
         return student.getName();
     }
