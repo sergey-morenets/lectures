7c8c773419e17cfdcbd64a765ea2eeef2b1b6a06
Sergiy <sergey.morenets@gmail.com>
2022-09-27 11:51:17 +0300

    Lecture 11. Retry pattern

--- lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
+++ lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
@@ -2,20 +2,31 @@ package capgemini.test.service;
 
 import capgemini.user.dto.StudentDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ExamService {
 
     private final RestTemplate restTemplate;
 
-    private String getStudentName(int studentId) {
-        //TODO how to use response class from other microservices ?
-        StudentDTO student = restTemplate.getForObject("http://user/students/" + studentId,
-                StudentDTO.class);
-        return student.getName();
+    private String getStudentName(int studentId) throws InterruptedException {
+        int maxAttempts = 3;
+        while (maxAttempts > 0) {
+            try {
+                StudentDTO student = restTemplate.getForObject("http://user/students/" + studentId,
+                        StudentDTO.class);
+                return student.getName();
+            } catch (Exception ex) {
+                maxAttempts--;
+                log.error(ex.getMessage(), ex);
+                Thread.sleep(1000);
+            }
+        }
+        return "N/A";
     }
 
 }
