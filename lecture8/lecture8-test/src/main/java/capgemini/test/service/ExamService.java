7c149ef6ef0bc342fd6663eb2d7ae252fb97602c
Sergiy <sergey.morenets@gmail.com>
2022-09-27 12:02:53 +0300

    Lecture 11. Integration with Resilience4j

--- lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
+++ lecture8/lecture8-test/src/main/java/capgemini/test/service/ExamService.java
@@ -1,10 +1,15 @@
 package capgemini.test.service;
 
 import capgemini.user.dto.StudentDTO;
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.retry.Retry;
+import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
@@ -13,20 +18,34 @@ public class ExamService {
 
     private final RestTemplate restTemplate;
 
-    private String getStudentName(int studentId) throws InterruptedException {
-        int maxAttempts = 3;
-        while (maxAttempts > 0) {
-            try {
-                StudentDTO student = restTemplate.getForObject("http://user/students/" + studentId,
-                        StudentDTO.class);
-                return student.getName();
-            } catch (Exception ex) {
-                maxAttempts--;
-                log.error(ex.getMessage(), ex);
-                Thread.sleep(1000);
-            }
-        }
-        return "N/A";
-    }
+    private String getStudentName(int studentId) throws Exception {
+//        int maxAttempts = 3;
+//        while (maxAttempts > 0) {
+//            try {
+//                StudentDTO student = restTemplate.getForObject("http://user/students/" + studentId,
+//                        StudentDTO.class);
+//                return student.getName();
+//            } catch (Exception ex) {
+//                maxAttempts--;
+//                log.error(ex.getMessage(), ex);
+//                Thread.sleep(1000);
+//            }
+//        }
+//        return "N/A";
 
+        Retry retry = Retry.ofDefaults("default");
+        Supplier<String> supplier = Decorators.ofSupplier(
+                        () -> restTemplate.getForObject("http://user/students/" + studentId,
+                                StudentDTO.class).getName())
+                .withRetry(retry)
+                .decorate();
+        // Or take student name from the cache
+        return Try.ofSupplier(supplier)
+                .recover(ex -> "N/A").get();
+
+//        String name = retry.executeCallable(() -> restTemplate.getForObject("http://user/students/" + studentId,
+//                StudentDTO.class).getName());
+//        return name;
+
+    }
 }
