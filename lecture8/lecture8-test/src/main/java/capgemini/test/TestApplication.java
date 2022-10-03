2594ad223cbed79ba910eb995b375da9675701ff
Sergiy <sergey.morenets@gmail.com>
2022-10-03 11:15:24 +0300

    Lecture 12. Fix detection of OpenFeign client.

--- lecture8/lecture8-test/src/main/java/capgemini/test/TestApplication.java
+++ lecture8/lecture8-test/src/main/java/capgemini/test/TestApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@ComponentScan("capgemini.user.api")
+@EnableFeignClients("capgemini.user.api")
 public class TestApplication {
 
     public static void main(String[] args) {
