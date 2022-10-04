eefdfc542c776ae4586cf2b4584de5e3431c8cb5
Sergiy <sergey.morenets@gmail.com>
2022-10-04 10:15:44 +0300

    Lecture 13. User photo task

---
+++ lecture13/src/main/java/capgemini/security/model/User.java
@@ -0,0 +1,10 @@
+package capgemini.security.model;
+
+/**
+ * User can have multiple photos
+ * Each photo file can take up to 10M
+ * 1. How to store
+ * 2. How to upload
+ */
+public class User {
+}
